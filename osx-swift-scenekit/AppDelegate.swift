db990d8fe5c55071d92ac43c5f760aa41d388103
Todd Crone <todd.crone@perceptivesoftware.com>
2015-06-29 10:36:32 -0400

    Initial Commit

---
+++ osx-swift-scenekit/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  osx-swift-scenekit
+//
+//  Created by Todd Crone on 6/29/15.
+//  Copyright (c) 2015 Todd Crone. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+    
+}
