b23de32ced82e34949e98bb48262065a38056bdf
Todd Crone <todd.crone@perceptivesoftware.com>
2015-06-30 10:18:24 -0400

    add physics

--- osx-swift-scenekit/GameScene.swift
+++ osx-swift-scenekit/GameScene.swift
@@ -1,6 +1,7 @@
 import Foundation
 import SceneKit
 import QuartzCore
+import SpriteKit
 
 
 class GameScene : SCNScene {
@@ -34,9 +35,45 @@ class GameScene : SCNScene {
             
             xPos += 0.66
         }
+        
+        
+        addSphereNode()
+        addFloorNode()
     }
     
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+    }
+    
+    func addSphereNode() {
+        let collisionBox = SCNBox(width: 10.0, height: 10.0, length: 10.0,
+            chamferRadius: 0)
+        
+        let sphereGeometry = SCNSphere(radius: 0.25)
+        sphereGeometry.firstMaterial?.diffuse.contents = NSColor.purpleColor()
+        
+        let sphereNode = SCNNode(geometry: sphereGeometry)
+        sphereNode.position = SCNVector3Make(0.0, 10.0, 0.0)
+        sphereNode.physicsBody?.physicsShape = SCNPhysicsShape(geometry: collisionBox, options: nil)
+        sphereNode.physicsBody = SCNPhysicsBody.dynamicBody()
+        sphereNode.physicsBody?.mass = 50
+        //sphereNode.physicsBody?.angularVelocityFactor = SCNVector3Zero
+//        sphereNode.physicsBody?.velocity = SCNVector3(x: 0.0, y: 0.0, z: 25.0)
+        sphereNode.physicsBody?.rollingFriction = 0.5
+        sphereNode.name = "bigSphere"
+        self.rootNode.addChildNode(sphereNode)
+    }
+    
+    func addFloorNode() {
+        var floorNode = SCNNode()
+        var floorGeometry = SCNFloor()
+        floorNode.geometry = floorGeometry
+        floorNode.position.y = -1.0
+        floorNode.physicsBody = SCNPhysicsBody.staticBody()
+        floorNode.physicsBody?.restitution = 0.75
+        floorNode.physicsBody?.rollingFriction = 0.5
+        
+        self.rootNode.addChildNode(floorNode)
+        
     }
 }
